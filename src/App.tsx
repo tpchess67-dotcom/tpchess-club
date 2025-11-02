d3a7b0f67e4d27ed5c33b0ecfd51113e0e76c8c0
tpchess67-dotcom <tpchess67@gmail.com>
2025-11-02 02:35:09 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/tpchess-club">
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
